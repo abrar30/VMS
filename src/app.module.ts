e7ea46ba0ac7dc2c1d05bbe1a48fea65fec434ec
abrar <abrarhussain@ifca.com.my>
2020-12-19 19:27:41 +0800

    added packages :package:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CustomersModule } from './customers/customers.module';
-import { VehicleModule } from './vehicles/vehicles.module';
 import { ConfigModule, ConfigService } from 'nestjs-config';
 import * as path from 'path';
-import { FirebaseStrategy } from './auth/firebase.strategy';
-import { FirebaseAuthStrategy } from './auth';
-import { FirebaseAdminCoreModule } from '@tfarras/nestjs-firebase-admin';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CustomersModule } from './customers/customers.module';
+import { VehicleModule } from './vehicles/vehicles.module';
 @Module({
   imports: [
     ConfigModule.load(
@@ -23,10 +20,7 @@ import { FirebaseAdminCoreModule } from '@tfarras/nestjs-firebase-admin';
       }),
     }),
     /// firebase jwt-passwort
-    FirebaseAdminCoreModule.forRootAsync({
-      useFactory: (config: ConfigService) => config.get('firebase'),
-      inject: [ConfigService],
-    }),
+
     CustomersModule,
     VehicleModule,
   ],
